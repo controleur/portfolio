dc126f314989df8c7947b125d62ab5743eaf72be
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-03 19:27:37 +0300

    styling apps in taskbar

--- src/components/Taskbar.svelte
+++ src/components/Taskbar.svelte
@@ -1,2 +1,11 @@
-<div class="w-full h-10 bg-white/75 absolute bottom-0">
+<div id="taskbar" class="absolute bottom-0 flex h-10 w-full bg-white/75">
+	<div id="startBtn" class="size-10 flex p-1">
+		<img src="/src/assets/icons/start-here-kde.svg" alt="Start" />
+	</div>
+	<div id="openedApps" class="flex gap-0.5">
+		<div class="size-10 p-1 flex"><img src="/src/assets/icons/files.svg" alt="Files" /></div>
+		<div class="size-10 p-1 flex"><img src="/src/assets/icons/files.svg" alt="Files" /></div>
+		<div class="size-10 p-1 flex"><img src="/src/assets/icons/files.svg" alt="Files" /></div>
+		<div class="size-10 p-1 flex"><img src="/src/assets/icons/files.svg" alt="Files" /></div>
+	</div>
 </div>
