ecddceba658746c94ccb049d22464ed3fe8ea59d
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-03 15:37:46 +0300

    first commit (sveltekit on docker with tailwind, hotreload for dev)

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+    watch: {
+      usePolling: true,
+      interval: 100
+    },
+    host: '0.0.0.0',
+    port: 5173
+  }
+});
