ccc5079cf382f491a1cfd1430eed4383c941ea4c
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-03 17:38:33 +0300

    use slot to render children

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-
-	let { children } = $props();
+	import Taskbar from '../components/Taskbar.svelte';
 </script>
 <div class="bg-[url(wallpapers/images/5120x2880.png)] min-h-screen bg-center">
-	{@render children()}
+	<slot />
 </div>
 
