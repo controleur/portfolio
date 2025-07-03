77e2e15513bbe074f8047b3c54d547f416eda4a5
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-03 17:48:01 +0300

    add blank taskbar

---
+++ src/components/Taskbar.svelte
@@ -0,0 +1,3 @@
+<div class="w-full h-10 bg-white/75 absolute bottom-0">
+
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Taskbar from '../components/Taskbar.svelte';
 </script>
 <div class="bg-[url(wallpapers/images/5120x2880.png)] min-h-screen bg-center">
+	<Taskbar />
 	<slot />
 </div>
 
