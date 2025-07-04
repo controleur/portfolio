05db5442d4bf9c702edbd343e50f0d9af9d7eb8a
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-04 21:34:06 +0300

    add tray layout (quick-actions), svg inline

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import Taskbar from '../components/Taskbar.svelte';
+	import TrayOpened from '../components/TrayOpened.svelte';
 </script>
 <div class="bg-[url(wallpapers/images/5120x2880.png)] min-h-screen bg-center">
 	<Taskbar />
+	<TrayOpened />
 	<!-- <slot /> calls childs -->
 </div>
 
